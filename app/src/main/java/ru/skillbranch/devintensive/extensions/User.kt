1fa05426bd239ca5d264cbdcab3b790f538c35ca
DariaKorzhueva <korzhueva.daria@gmail.com>
2019-07-04 16:40:42 +0700

    Fix truncate

--- app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
@@ -26,18 +26,23 @@ fun User.toUserView(): UserView {
 
 /* Усечение строки */
 fun String.truncate(number: Int = 16): String {
-    /* Если длина строки меньше количества убираемых символов */
-    if (this.length - number < 0) {
+
+    /* Удаление лишних пробелов в конце строки*/
+    var truncString = this.trim(' ')
+
+    /* Если длина строки без пробелов меньше количества убираемых символов */
+    if (truncString.length < number) {
         /* Если последний символ - пробел, он удаляется */
         if (this[this.length - 1].toString() == " ")
-            return this.trim()
+            return this.trim(' ')
         else
             return this
     }
 
-   var truncString = this.dropLast(this.length - number)
+    /* Удаление последних символов, не являющихся пробелами */
+   truncString = this.dropLast(this.length - number)
 
-    /* Если последний символ - пробел, он удаляется*/
+    /* Если последний символ после усечения - пробел, то он удаляется*/
     if (this[number - 1].toString() == " ") {
         truncString = truncString.trim()
     }
