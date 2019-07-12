68e6b8a8fba72406c7f44475717e2ee7b670446a
DariaKorzhueva <korzhueva.daria@gmail.com>
2019-07-12 19:47:09 +0700

    Add validate

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -92,7 +92,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, TextView.OnEdito
 
     override fun onClick(v: View?) {
         if (v?.id == R.id.iv_send) {
-            val (phrase, color) = benderObj.listenAnswer(messageEt.text.toString().toLowerCase())
+            val (phrase, color) = benderObj.listenAnswer(messageEt.text.toString())
             messageEt.setText("")
             var (r, g, b) = color
             benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
@@ -104,7 +104,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, TextView.OnEdito
 
     override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?): Boolean {
         if (actionId == EditorInfo.IME_ACTION_DONE) {
-            val (phrase, color) = benderObj.listenAnswer(messageEt.text.toString().toLowerCase())
+            val (phrase, color) = benderObj.listenAnswer(messageEt.text.toString())
             messageEt.setText("")
             var (r, g, b) = color
             benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -11,58 +11,88 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
     }
 
     fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
-        return if(question.answers.contains(answer)){
-            question = question.nextQuestion()
-            "Отлично - ты справился\n${question.question}" to status.color
-        }else{
-            if(status == Status.CRITICAL){
-                status = Status.NORMAL
-                question = Question.NAME
-                "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
+        val valid = validate(answer, question)
+        if (valid == "") {
+            if (question.answers.contains(answer)) {
+                question = question.nextQuestion()
+                return "Отлично - ты справился\n${question.question}" to status.color
+            } else {
+                if (status == Status.CRITICAL) {
+                    status = status.nextStatus()
+                    question = Question.NAME
+                    return "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
+                } else {
+                    status = status.nextStatus()
+                    return "Это неправильный ответ\n${question.question}" to status.color
+                }
             }
-            else {
-                status = status.nextStatus()
-                "Это неправильный ответ\n${question.question}" to status.color
-            }
+        } else {
+            return "$valid\n${question.question}" to status.color
         }
     }
 
-    enum class Status(val color: Triple<Int,Int,Int>) {
+    fun validate(answer: String, question: Question): String {
+        when (question) {
+            Question.NAME -> {
+                if (answer[0] != answer[0].toUpperCase())
+                    return "Имя должно начинаться с заглавной буквы"
+            }
+            Question.PROFESSION -> {
+                if (answer[0] != answer[0].toLowerCase())
+                    return "Профессия должна начинаться со строчной буквы"
+            }
+            Question.MATERIAL -> {
+                if (Regex("[0-9] ").find(answer) != null)
+                    return "Материал не должен содержать цифр"
+            }
+            Question.BDAY -> {
+                if (Regex("[^0-9]+").find(answer) != null)
+                    return "Год моего рождения должен содержать только цифры"
+            }
+            Question.SERIAL -> {
+                if (Regex("[0-9]{8}").find(answer) != null)
+                    return "Серийный номер содержит только цифры, и их 7"
+            }
+        }
+
+        return ""
+    }
+
+    enum class Status(val color: Triple<Int, Int, Int>) {
         NORMAL(Triple(255, 255, 255)),
         WARNING(Triple(255, 120, 0)),
         DANGER(Triple(255, 60, 60)),
         CRITICAL(Triple(255, 0, 0));
 
-        fun nextStatus():Status{
-            return if(this.ordinal < values().lastIndex){
+        fun nextStatus(): Status {
+            return if (this.ordinal < values().lastIndex) {
                 values()[this.ordinal + 1]
-            }
-            else{
+            } else {
                 values()[0]
             }
         }
     }
 
     enum class Question(val question: String, val answers: List<String>) {
-        NAME("Как меня зовут?", listOf("Бендер", "bender")) {
+        NAME("Как меня зовут?", listOf("Бендер", "Bender")) {
             override fun nextQuestion(): Question = PROFESSION
         },
-        PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")){
+        PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")) {
             override fun nextQuestion(): Question = MATERIAL
         },
-        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")){
+        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")) {
             override fun nextQuestion(): Question = BDAY
         },
-        BDAY("Когда меня создали?", listOf("2993")){
+        BDAY("Когда меня создали?", listOf("2993")) {
             override fun nextQuestion(): Question = SERIAL
         },
-        SERIAL("Мой серийный номер?", listOf("2716057")){
+        SERIAL("Мой серийный номер?", listOf("2716057")) {
             override fun nextQuestion(): Question = IDLE
         },
-        IDLE("На этом все, вопросов больше нет", listOf()){
+        IDLE("На этом все, вопросов больше нет", listOf()) {
             override fun nextQuestion(): Question = IDLE
         };
 
-        abstract fun nextQuestion():Question
+        abstract fun nextQuestion(): Question
     }
 }
