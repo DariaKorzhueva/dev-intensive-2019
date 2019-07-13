156a881f30eb414d40567eaee8010fdd19f61a46
DariaKorzhueva <korzhueva.daria@gmail.com>
2019-07-13 17:45:46 +0700

    Fix checking of keyboard

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -1,12 +1,12 @@
 package ru.skillbranch.devintensive.extensions
 
 import android.app.Activity
+import android.content.Context
 import android.graphics.Rect
 import android.view.inputmethod.InputMethodManager
-import android.R.attr.top
-import android.R.attr.bottom
-import android.opengl.ETC1.getHeight
-
+import android.util.TypedValue
+import android.view.View
+import kotlin.math.round
 
 /* Скрытие клавиатуры */
 fun Activity.hideKeyboard() {
@@ -19,25 +19,30 @@ fun Activity.hideKeyboard() {
     imm.hideSoftInputFromWindow(view!!.getWindowToken(), 0)
 }
 
-fun Activity.checkKeyboard(): Boolean {
-    val rootView = this.currentFocus
-    val rect = Rect().apply { rootView.getWindowVisibleDisplayFrame(this) }
-
-    val screenHeight = rootView.height
-
-    val keypadHeight = screenHeight - rect.bottom
-
-    if (keypadHeight > screenHeight * 0.15) {
-        return true
-    }
-    return false
-
+/* Конвертация dp в px */
+fun Context.convertDpToPx(dp: Float): Float {
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, this.resources.displayMetrics)
 }
 
+/* Проверка конфигурации клавиатуры */
+fun Activity.checkKeyboard(): Boolean {
+    val rootView = this.findViewById<View>(android.R.id.content)
+    val r = Rect()
+
+    rootView.getWindowVisibleDisplayFrame(r)
+
+    val heightDiff = rootView.height - r.height()
+    val marginOfError = round(this.convertDpToPx(50F))
+
+    return heightDiff > marginOfError
+}
+
+/* Проврека на открытую клавиатуры */
 fun Activity.isKeyboardOpen(): Boolean {
     return checkKeyboard()
 }
 
+/* Проврека на закрытую клавиатуры */
 fun Activity.isKeyboardClosed(): Boolean {
     return !checkKeyboard()
 }
