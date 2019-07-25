b652d5715bd87befa19a2485402dabff2d0fd74e
DariaKorzhueva <korzhueva.daria@gmail.com>
2019-07-26 05:02:37 +0700

    Fix getProfile

--- app/src/main/java/ru/skillbranch/devintensive/models/Profile.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Profile.kt
@@ -10,8 +10,8 @@ data class Profile(
     val rating: Int = 0,
     val respect: Int = 0
 ) {
-    val nickName:String = "John Doe"
-        //get() = Utils.transliteration("$firstName $lastName","_")
+    val nickName:String
+        get() = Utils.transliteration("$firstName $lastName","_")
     val rank: String = "Junior Android Developer"
 
     fun toMap(): Map<String,Any> = mapOf(
