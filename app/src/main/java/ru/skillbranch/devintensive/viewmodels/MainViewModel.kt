7a356467d6ed982395aa65a313fd1991c0b06629
DariaKorzhueva <korzhueva.daria@gmail.com>
2019-09-06 02:15:28 +0700

    Fix mismatches

--- app/src/main/java/ru/skillbranch/devintensive/viewmodels/MainViewModel.kt
+++ app/src/main/java/ru/skillbranch/devintensive/viewmodels/MainViewModel.kt
@@ -3,14 +3,12 @@ package ru.skillbranch.devintensive.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
-import ru.skillbranch.devintensive.extensions.mutableLiveData
 import ru.skillbranch.devintensive.models.data.ChatItem
 import ru.skillbranch.devintensive.repositories.ChatRepository
-import ru.skillbranch.devintensive.utils.DataGenerator
 
 class MainViewModel : ViewModel() {
-    private val chatReposity = ChatRepository
-    private val chats = Transformations.map(chatReposity.loadChats()){chats->
+    private val chatRepository = ChatRepository
+    private val chats = Transformations.map(chatRepository.loadChats()){ chats->
 
         return@map chats.filter { !it.isArchived }.map{it.toChatItem()}.sortedBy { it.id.toInt() }
     }
@@ -20,14 +18,14 @@ class MainViewModel : ViewModel() {
     }
 
     fun addToArchive(chatId: String) {
-        val chat = chatReposity.find(chatId)
+        val chat = chatRepository.find(chatId)
         chat ?: return
-        chatReposity.update(chat.copy(isArchived = true))
+        chatRepository.update(chat.copy(isArchived = true))
     }
 
     fun restoreFromArchive(chatId: String){
-        val chat = chatReposity.find(chatId)
+        val chat = chatRepository.find(chatId)
         chat ?: return
-        chatReposity.update(chat.copy(isArchived = false))
+        chatRepository.update(chat.copy(isArchived = false))
     }
 }
