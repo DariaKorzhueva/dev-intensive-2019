72f97989ecf57bc723b37b7cd8e5d2a12fb5b54b
DariaKorzhueva <korzhueva.daria@gmail.com>
2019-09-09 01:44:24 +0700

    Add item_chat_archive

--- app/src/main/java/ru/skillbranch/devintensive/ui/adapters/ChatAdapter.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/adapters/ChatAdapter.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_chat_archive.*
 import kotlinx.android.synthetic.main.item_chat_group.*
 import kotlinx.android.synthetic.main.item_chat_single.*
 import ru.skillbranch.devintensive.R
@@ -33,6 +34,7 @@ class ChatAdapter(val listener: (ChatItem) -> Unit) : RecyclerView.Adapter<ChatA
         val inflater = LayoutInflater.from(parent.context)
 
         return when (viewType) {
+            ARCHIVE_TYPE -> ArchiveViewHolder(inflater.inflate(R.layout.item_chat_archive, parent, false))
             SINGLE_TYPE -> SingleViewHolder(inflater.inflate(R.layout.item_chat_single, parent, false))
             GROUP_TYPE -> GroupViewHolder(inflater.inflate(R.layout.item_chat_group, parent, false))
             else -> SingleViewHolder(inflater.inflate(R.layout.item_chat_single, parent, false))
@@ -143,6 +145,36 @@ class ChatAdapter(val listener: (ChatItem) -> Unit) : RecyclerView.Adapter<ChatA
             with(tv_message_author) {
                 visibility = if (item.messageCount > 0) View.VISIBLE else View.GONE
                 text = item.author
+            }
+
+            itemView.setOnClickListener {
+                listener.invoke(item)
+            }
+
+        }
+    }
+
+    inner class ArchiveViewHolder(convertView: View) : ChatItemViewHolder(convertView), LayoutContainer{
+        override fun bind(item: ChatItem, listener: (ChatItem) -> Unit) {
+            tv_title_archive.text = item.title
+            tv_message_archive.text = item.shortDescription
+
+            with(tv_date_archive) {
+                visibility = if (item.lastMessageDate != null) View.VISIBLE else View.GONE
+                text = item.lastMessageDate
+            }
+
+            with(tv_counter_archive) {
+                visibility = if (item.messageCount > 0) View.VISIBLE else View.GONE
+                text = item.messageCount.toString()
+            }
+
+            tv_title_archive.text = item.title
+            tv_message_archive.text = item.shortDescription
+
+            with(tv_message_author_archive) {
+                visibility = if (item.messageCount > 0) View.VISIBLE else View.GONE
+                text = "@${item.author}"
             }
 
             itemView.setOnClickListener {
